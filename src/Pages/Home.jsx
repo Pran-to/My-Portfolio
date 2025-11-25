85e198031d0d4aa5e3a654496f4062305e410838
pran-to <prantochakrobortty24@gmail.com>
2025-11-26 02:34:50 +0600

    add animation

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,10 +1,13 @@
+import Background from "../components/Background";
 import ThemeToggle from "../components/ThemeToggle";
 
 const Home = () => {
     return (
-        <div className='main-h-screen bg-background text-foreground overflow-x-hidden '>
+        <div className='min-h-screen bg-background text-foreground overflow-x-hidden '>
            {/* Theme  Toggle  */}
            <ThemeToggle></ThemeToggle>
+           {/* Background Effects  */}
+           <Background></Background>
         </div>
     );
 };
--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -7,7 +7,7 @@ const ThemeToggle = () => {
     useEffect(()=>{
         const  storeTheme = localStorage.getItem('theme')
         if(storeTheme ==  'dark'){
-            setDarkMode(true);
+            setDarkMode (true);
             document.documentElement.classList.add('dark')
         }
         else{
@@ -31,9 +31,9 @@ const ThemeToggle = () => {
     }
 
     return (
-        <button onClick={toggleTheme}>
+        <button onClick={toggleTheme} className="fixed max-sm:hidden top-5 right-5 z-50 p-2 rounded-full transition-colors duration-300 focus:outline-hidden">
             {
-                isDarkMode ? <Sun className="h-6 w-6 text-yellow-300"></Sun>  : <Moon className="h-6 w-6 text-blue-900"></Moon>
+                isDarkMode ? <Sun className="h-6 w-6 text-yellow-300"></Sun>  : <Moon className="h-6 w-6 text-blue-900 fill-blue-950"></Moon>
             }
         </button>
     );
