bb1140e23d292ace10b3f1e74120ee10027bfd3e
pran-to <prantochakrobortty24@gmail.com>
2026-01-06 23:19:52 +0600

    Abot Section

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,22 +1,25 @@
+import AboutSection from "../components/AboutSection";
 import Background from "../components/Background";
 import HeroSection from "../components/HeroSection";
 import Navbar from "../components/Navbar";
 import ThemeToggle from "../components/ThemeToggle";
 
 const Home = () => {
-    return (
-        <div className='min-h-screen bg-background text-foreground overflow-x-hidden '>
-           {/* Theme  Toggle  */}
-           <ThemeToggle></ThemeToggle>
-           {/* Background Effects  */}
-           <Background></Background>
-           {/* NaveBar  */}
-           <Navbar/>
-           <HeroSection></HeroSection>
+  return (
+    <div className="min-h-screen bg-background text-foreground overflow-x-hidden ">
+      {/* Theme  Toggle  */}
+      <ThemeToggle></ThemeToggle>
+      {/* Background Effects  */}
+      <Background></Background>
+      {/* NaveBar  */}
+      <Navbar />
 
-
-        </div>
-    );
+      <main>
+        <HeroSection></HeroSection>
+        <AboutSection></AboutSection>
+      </main>
+    </div>
+  );
 };
 
 export default Home;
